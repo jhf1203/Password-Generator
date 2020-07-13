d0728a144c90e3c4f808bde907a9d2054495f9a3
Jim Faulkner <jhf1203@gmail.com>
2020-07-13 10:19:44 -0400

    Site functional with captions

--- Develop/script.js
+++ Develop/script.js
@@ -13,91 +13,85 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Variables that the user confirms
+// Function to generate the password begins here
+function generatePassword() {
 
-  // var pwLength=prompt("How many characters should your password contain?");
-  // var i = pwLength
+    // This is where the user inputs their password length, and we ensure that it is converted into a number rather than a string.
+    var pwLength= parseInt(prompt("How many characters should your password contain (between 8 - 128"));
 
-  // if (pwLength >= 8 && pwLength <= 128) {
+     // This ensures that the password falls within the character length parameters prior to getting the character type criteria.
+    if (pwLength >= 8 && pwLength <= 128) {
 
-    // var pwUppers = confirm("Would you like for your password to contain upper-case letters?");
-    // var pwLowers = confirm("Would you like for your password to contain lower-case letters?");
-    // var pwNumbers = confirm("Would you like for your password to contain numbers?");
-    // var pwSymbols = confirm("Would you like for your password to contain special characters?");
+      var pwUppers = confirm("Would you like for your password to contain upper-case letters?  Please select 'OK' for yes or 'Cancel' for no.");
+      var pwLowers = confirm("Would you like for your password to contain lower-case letters?  Please select 'OK' for yes or 'Cancel' for no.");
+      var pwNumbers = confirm("Would you like for your password to contain numbers?  Please select 'OK' for yes or 'Cancel' for no.");
+      var pwSymbols = confirm("Would you like for your password to contain special characters?  Please select 'OK' for yes or 'Cancel' for no.");
 
-  // } else {
+    } else {
 
-    // alert("Sorry, your password must be between 8 and 128 characters in length.  Please refresh the page and try again.");
+      alert("Sorry, your password must be between 8 and 128 characters in length.  Click 'Generate Password' and try again.");
 
-  //  }
-
-
-// Array of answers from the above questions
-// var answers=[pwLowers, pwUppers, pwNumbers, pwSymbols];
-
-// Arrays containing characters for potential passwords
-var listUppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var listLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var listNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-var listSymbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "|", "]", "}", "[", "{", ";", ":", "/", "?", ".", ">", ",", "<"];
-
-// Variables constituting a random selection from each of the above variables
-
-var pickUpper = listUppers[Math.floor(Math.random() *listUppers.length)]
-var pickLower = listLowers[Math.floor(Math.random() *listLowers.length)]
-var pickNumber = listNumbers[Math.floor(Math.random() *listNumbers.length)]
-var pickSymbol = listSymbols[Math.floor(Math.random() * listSymbols.length)]
-
-// console.log(pickUpper)
-// console.log(pickLower)
-// console.log(pickNumber)
-// console.log(pickSymbol)
-
-// Variable collection of all possible answer choices, creating an array of one randomly selected object from each selected array.
-
-var YYYY = [pickUpper, pickLower, pickNumber, pickSymbol]
-var YYYN = [pickUpper, pickLower, pickNumber]
-var YYNY = [pickUpper, pickLower, pickSymbol]
-var YYNN = [pickUpper, pickLower]
-var YNYY = [pickUpper, pickNumber, pickSymbol]
-var YNYN = [pickUpper, pickNumber]
-var YNNY = [pickUpper, pickSymbol]
-var YNNN = pickUpper
-var NYYY = [pickLower, pickNumber, pickSymbol]
-var NYYN = [pickLower, pickNumber]
-var NYNY = [pickLower, pickSymbol]
-var NYNN = pickLower
-var NNYY = [pickNumber, pickSymbol]
-var NNYN = pickNumber
-var NNNY = pickSymbol
-
-// console.log(YYYY)
+      }
 
 
+  // Arrays containing characters for potential passwords
+  var listUppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var pickRandom = YNNY[Math.floor(Math.random() * YNNY.length)]
+  var listLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// console.log(pickRandom.repeat(10))
+  var listNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-// function finalChoice () {
+  var listSymbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "|", "]", "}", "[", "{", ";", ":", "/", "?", ".", ">", ",", "<"];
 
-//   console.log(pickRandom.repeat(10))
+  // The below array will house all potential options for each password character based on the user's answers in lines 25-28.
+  var chosenOptions = [];
 
-// }
+  // the four "if" statements below move the appropriate password character options into the chosenOptions array
+  if (pwUppers === true) {
 
-// finalChoice
+    chosenOptions = chosenOptions.concat(listUppers)
+    
+  };
 
-for (i = 0; i < 20; i++) {
+  if (pwLowers === true) {
 
-  console.log(pickRandom)
-  console.log(pickRandom)
-  console.log(pickRandom)
-  console.log(pickRandom)
-  console.log(pickRandom)
+    chosenOptions = chosenOptions.concat(listLowers)
+
+  };
+
+  if (pwNumbers === true) {
+
+    chosenOptions = chosenOptions.concat(listNumbers)
+
+  };
+
+  if (pwSymbols === true) {
+
+    chosenOptions = chosenOptions.concat(listSymbols)
+
+  };
+
+  // This is where the password is generated
+  var generatePassword = ""
+
+  for (var i = 0; i < pwLength; i++) {
+
+    // Here is where the characters are selected to go into the actual password
+    generatePassword += chosenOptions[Math.floor(Math.random() * chosenOptions.length)]
+
+  }
+
+  return generatePassword
 
 }
+
+
+
+
+
+
+
+
+
 
 
